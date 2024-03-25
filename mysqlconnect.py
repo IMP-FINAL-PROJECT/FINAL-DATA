6c4987cd190b3177af0d927a9cbabb2f771074d6
KOH JOOWON <joowonkoh@naver.com>
2024-03-25 21:15:14 +0900

    FP-86 feat : mysql query 조회 함수 구현

--- mysqlconnect.py
+++ mysqlconnect.py
@@ -2,30 +2,26 @@ import pymysql
 from dotenv import load_dotenv
 import os
 
-
+# 환경 변수 로드
 load_dotenv()
-# MySQL 데이터베이스 연결 설정
+
+# 연결 설정  
 conn = pymysql.connect(
-    host=os.environ.get("Server_IP"),    # MySQL 서버 주소
-    user=os.environ.get("MYSQL_USER"),    # MySQL 사용자 이름
-    password=os.environ.get("MYSQL_PASSWORD"),  # MySQL 사용자 비밀번호
-    db='final',        # 접속할 데이터베이스 이름
-    charset='utf8mb4',    # 문자 인코딩 설정 
-    port=3306,    # 포트 번호
-)
+        host=os.environ.get("Server_IP"),            # MySQL 서버 주소
+        user=os.environ.get("MYSQL_USER"),           # MySQL 사용자 이름
+        password=os.environ.get("MYSQL_PASSWORD"),   # MySQL 사용자 비밀번호
+        db='final',                                  # 접속할 데이터베이스 이름
+        charset='utf8mb4',                           # 문자 인코딩 설정
+        port=3306                                    # 포트 번호
+    )
 
-try:
-    # 커서 생성
-    with conn.cursor() as cursor:
-        # SQL 쿼리 실행
-        sql = "SELECT * FROM user"  # 데이터를 가져올 테이블의 이름을 'your_table' 대신 사용
-        cursor.execute(sql)
-
-        # 결과 가져오기
-        result = cursor.fetchall()
-        for row in result:
-            print(row)  # 각 행의 데이터 출력
-
-finally:
-    # 데이터베이스 연결 종료
-    conn.close()
+# MySQL 데이터베이스 연결 및 쿼리 실행 함수
+def execute_query(query):
+    
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(query)  # 쿼리 실행
+            result = cursor.fetchall()  # 모든 결과 행 가져오기
+            return result  # 결과 반환
+    finally:
+        conn.close()  # 데이터베이스 연결 종료
