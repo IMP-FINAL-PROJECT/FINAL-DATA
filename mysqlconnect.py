ae8c9d0fb4dd8ee6325770591a038b6b0ac14288
KOH JOOWON <joowonkoh@naver.com>
2024-04-10 21:14:40 +0900

    FP-147 feat : mysql 연결 함수구현

--- mysqlconnect.py
+++ mysqlconnect.py
@@ -27,6 +27,25 @@ def execute_query(query):
     finally:
         conn.close()  # 데이터베이스 연결 종료
 
+def execute_val_query(query,val):
+    conn = pymysql.connect(
+        host=os.environ.get("Server_IP"),            # MySQL 서버 주소
+        user=os.environ.get("MYSQL_USER"),           # MySQL 사용자 이름
+        password=os.environ.get("MYSQL_PASSWORD"),   # MySQL 사용자 비밀번호
+        db='final',                                  # 접속할 데이터베이스 이름
+        charset='utf8mb4',                           # 문자 인코딩 설정
+        port=3306                                    # 포트 번호
+    )
+
+    
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(query,val)  # 쿼리 실행
+            result = cursor.fetchall()  # 모든 결과 행 가져오기
+            return result  # 결과 반환
+    finally:
+        conn.close()  # 데이터베이스 연결 종료
+
 
 def executemany_query(query,val):
     conn = pymysql.connect(
@@ -66,4 +85,31 @@ def execute_insert_query(query):
             conn.commit()
     finally:
         conn.close()  # 데이터베이스 연결 종료     
+
+def excutemany_delete_query(query,val_list):
+    conn = pymysql.connect(
+        host=os.environ.get("Server_IP"),            # MySQL 서버 주소
+        user=os.environ.get("MYSQL_USER"),           # MySQL 사용자 이름
+        password=os.environ.get("MYSQL_PASSWORD"),   # MySQL 사용자 비밀번호
+        db='final',                                  # 접속할 데이터베이스 이름
+        charset='utf8mb4',                           # 문자 인코딩 설정
+        port=3306                                    # 포트 번호
+    )   
+    
+    try:
+        with conn.cursor() as cursor:
+            # existing_entries_list 순회
+            for id, date in val_list:
+                # DELETE 쿼리 실행
+                cursor.execute(query, (id, date))
+            
+            # 변경 사항 저장
+            conn.commit()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        conn.rollback()  # 오류 발생 시 롤백
+
+    finally:
+        conn.close()  # 연결 닫기
 
